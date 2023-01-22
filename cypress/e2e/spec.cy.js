9a4278eb1d1a1481d9b851f96b2491cd2c063f9e
Aleq-de-Villiers <123024509+Aleq-de-Villiers@users.noreply.github.com>
2023-01-22 22:06:13 +0000

    Build: (e6d3fcf) Update main.yml

--- cypress/e2e/spec.cy.js
+++
@@ -1,6 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get("[id=test_head]").invoke("text").should("eq" ,"this is a test")
-  })
-})
